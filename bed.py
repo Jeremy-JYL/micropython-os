6d082bcd53dedd851f2919141c87b3381cd3c559
Jeremy@JYL <108569568+Jeremy-JYL@users.noreply.github.com>
2024-04-10 17:44:34 +0700

    Update bed.py with a run function so it is easier for REPL

--- bed.py
+++ bed.py
@@ -1,6 +1,9 @@
 # Simple BASIC like text editor
 
+import sys
+
 # Where Data Store
+global data
 data = {}
 
 # Save
@@ -21,41 +24,44 @@ def load(path):
             data[line_counter] = i.strip()
             line_counter += 10
 
-print("BED - BASIC Editor")
+def run():
+    global data
+    print("BED - BASIC Editor")
 
-while True:
-    try:
-        words = input("> ") # Input
-        args = words.split() # Split
-        if not words:
-            pass
-        elif args[0].isdigit():
-            data[int(args[0])] = " ".join(args[1:]) # Storing text with line number
-            data = {key: data[key] for key in sorted(data)} # Order it
-        # List command
-        elif args[0].upper() == "LIST":
-            # Example: list [LINE NUMBER]
-            if len(args) > 1:
-                print(data[int(args[1])])
-            # Example: list
+    while True:
+        try:
+            words = input("> ") # Input
+            args = words.split() # Split
+            if not words:
+                pass
+            elif args[0].isdigit():
+                data[int(args[0])] = " ".join(args[1:]) # Storing text with line number
+                data = {key: data[key] for key in sorted(data)} # Order it
+            # List command
+            elif args[0].upper() == "LIST":
+                # Example: list [LINE NUMBER]
+                if len(args) > 1:
+                    print(data[int(args[1])])
+                # Example: list
+                else:
+                    for i in data:
+                        print(str(i) + " " + data[i])
+            # Save
+            elif args[0].upper() == "SAVE":
+                save(args[1])
+            # Load
+            elif args[0].upper() == "LOAD":
+                load(args[1])
+            # New
+            elif args[0].upper() == "NEW":
+                data = {}
+            elif args[0].upper() == "EXIT":
+                sys.exit()
             else:
-                for i in data:
-                    print(str(i) + " " + data[i])
-        # Save
-        elif args[0].upper() == "SAVE":
-            save(args[1])
-        # Load
-        elif args[0].upper() == "LOAD":
-            load(args[1])
-        # New
-        elif args[0].upper() == "NEW":
-            data = {}
-        else:
-            if not data:
-                data[0] = words
-            else:
-                data[list(data.keys())[-1] + 1] = words
-    except KeyboardInterrupt:
-        break
-    except Exception as e: print(e)
-
+                if not data:
+                    data[0] = words
+                else:
+                    data[list(data.keys())[-1] + 1] = words
+        except KeyboardInterrupt:
+            break
+        except Exception as e: print(e)
